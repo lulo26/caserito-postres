ed36e4ba609ffa97da9b2ea7ba8ee6c881d61422
lulo <lopez.gallego2611@gmail.com>
2025-03-29 11:40:39 -0500

    small changes

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,3 +1,19 @@
 <div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    <h1>Posts</h1>
+    <a href="{{ route('productos.create') }}">Crear Post</a>
+    <ul>
+        @foreach ($posts as $post)
+            <li>
+                {{ $post->title }}
+                <a href="{{ route('productos.edit', $post) }}">Editar</a>
+                <form action="{{ route('productos.destroy', $post) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Eliminar</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+@endsection
+
 </div>
